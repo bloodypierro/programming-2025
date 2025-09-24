59be3be991e64022b524edf04fad65fa7714b88d
bloodypierro <internetwarningg@gmail.com>
2025-09-24 13:29:55 +0200

    11
    1

---
+++ Test Concsole Projectfor1Matob/Test Concsole Projectfor1Matob/Test Concsole Projectfor1Matob.cpp
@@ -0,0 +1,20 @@
+﻿
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+
+int main()
+{
+    const double P = 3.14;
+    double radius;
+    cout << "Radius sferi:";
+    cin >> radius;
+    double Ploshyad = 4 * P * pow(radius, 2);
+    double volume = (4.0 / 3.0) * P * pow(radius, 3);
+    cout << "\n Result:" << endl;
+    cout << "Radius sferi:" << radius << endl;
+    cout << "Ploshyad poverhnosti:" << Ploshyad << endl;
+    cout << "Obiem sferi:" << volume << endl;
+    return 0;
+}   
